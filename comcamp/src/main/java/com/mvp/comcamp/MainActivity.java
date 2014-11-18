02cee6dcd48a382243521b3464fe24c33d119557
YoungjaeKim <youngjae.kim@gmail.com>
2014-11-18 19:09:21 +0900

    Initial Commit

---
+++ comcamp/src/main/java/com/mvp/comcamp/MainActivity.java
@@ -0,0 +1,319 @@
+package com.mvp.comcamp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.microsoft.windowsazure.mobileservices.*;
+import com.microsoft.windowsazure.notifications.NotificationsManager;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
+
+import static com.microsoft.windowsazure.mobileservices.MobileServiceQueryOperations.val;
+
+public class MainActivity extends Activity {
+
+	public static MobileServiceClient mClient;
+
+	/**
+	 * Mobile Service Table used to access data
+	 */
+	private MobileServiceTable<ToDoItem> mToDoTable;
+
+	/**
+	 * Adapter to sync the items list with the view
+	 */
+	private ToDoItemAdapter mAdapter;
+
+	/**
+	 * EditText containing the "New ToDo" text
+	 */
+	private EditText mTextNewToDo;
+
+	/**
+	 * Progress spinner to use for table operations
+	 */
+	private ProgressBar mProgressBar;
+
+	public static final String SENDER_ID = "0000000000"; // YOUR_GOOGLE_GCM_PROJECT_NUMBER
+	private GoogleCloudMessaging gcm;
+	private String regid;
+	private Context context;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mProgressBar = (ProgressBar) findViewById(R.id.loadingProgressBar);
+
+		// Initialize the progress bar
+		mProgressBar.setVisibility(ProgressBar.GONE);
+
+		try {
+			// Create the Mobile Service Client instance, using the provided
+			// Mobile Service URL and key
+			mClient = new MobileServiceClient(
+					"https://***YOUR_ZUMO_DOMAIN***.azure-mobile.net/",
+					"***DOWNLOAD_FROM_AZURE_ZUMO_DASHBOARD***",
+					this).withFilter(new ProgressFilter());
+
+			// Get the Mobile Service Table instance to use
+			mToDoTable = mClient.getTable(ToDoItem.class);
+
+			mTextNewToDo = (EditText) findViewById(R.id.textNewToDo);
+
+			// Create an adapter to bind the items with the view
+			mAdapter = new ToDoItemAdapter(this, R.layout.row_list_to_do);
+			ListView listViewToDo = (ListView) findViewById(R.id.listViewToDo);
+			listViewToDo.setAdapter(mAdapter);
+
+			// Load the items from the Mobile Service
+			refreshItemsFromTable();
+
+		} catch (MalformedURLException e) {
+			createAndShowDialog(new Exception("There was an error creating the Mobile Service. Verify the URL"), "Error");
+		}
+		NotificationsManager.handleNotifications(this, SENDER_ID, MyHandler.class);
+
+		context = getApplicationContext();
+		gcm = GoogleCloudMessaging.getInstance(this);
+		if (regid == null || "".equals(regid)) {
+			registerInBackground();
+		}
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		if (id == R.id.menu_refresh) {
+			refreshItemsFromTable();
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * Registers mobile services client to receive GCM push notifications
+	 *
+	 * @param gcmRegistrationId The Google Cloud Messaging session Id returned
+	 *                          by the call to GoogleCloudMessaging.register in NotificationsManager.handleNotifications
+	 */
+	public void registerForPush(String gcmRegistrationId) {
+		String[] tags = {null};
+	}
+
+	/**
+	 * Registers the application with GCM servers asynchronously.
+	 * <p/>
+	 * Stores the registration ID and app versionCode in the application's
+	 * shared preferences.
+	 */
+	private void registerInBackground() {
+		new AsyncTask() {
+			@Override
+			protected void onPostExecute(Object o) {
+				super.onPostExecute(o);
+			}
+
+			@Override
+			protected Object doInBackground(Object[] objects) {
+				String msg;
+				try {
+					if (gcm == null) {
+						gcm = GoogleCloudMessaging.getInstance(context);
+					}
+					regid = gcm.register(MainActivity.SENDER_ID);
+					msg = "Device registered, registration ID=" + regid;
+					String [] tags = {null};
+					mClient.getPush().register(regid, tags, new RegistrationCallback() {
+						@Override
+						public void onRegister(Registration registration, Exception exception) {
+							if (exception != null) {
+								// handle error
+							}
+						}});
+
+				} catch (IOException ex) {
+					msg = "Error :" + ex.getMessage();
+					// If there is an error, don't just keep trying to register.
+					// Require the user to click a button again, or perform
+					// exponential back-off.
+				}
+				return msg;
+			}
+		}.execute(null, null, null);
+	}
+	/**
+	 * Mark an item as completed
+	 *
+	 * @param item The item to mark
+	 */
+	public void checkItem(ToDoItem item) {
+		if (mClient == null) {
+			return;
+		}
+
+		// Set the item as completed and update it in the table
+		item.setComplete(true);
+
+		mToDoTable.update(item, new TableOperationCallback<ToDoItem>() {
+
+			public void onCompleted(ToDoItem entity, Exception exception, ServiceFilterResponse response) {
+				if (exception == null) {
+					if (entity.isComplete()) {
+						mAdapter.remove(entity);
+					}
+				} else {
+					createAndShowDialog(exception, "Error");
+				}
+			}
+
+		});
+	}
+
+	/**
+	 * Add a new item
+	 *
+	 * @param view The view that originated the call
+	 */
+	public void addItem(View view) {
+		if (mClient == null) {
+			return;
+		}
+
+		// Create a new item
+		ToDoItem item = new ToDoItem();
+
+		item.setText(mTextNewToDo.getText().toString());
+		item.setComplete(false);
+
+		// Insert the new item
+		mToDoTable.insert(item, new TableOperationCallback<ToDoItem>() {
+
+			public void onCompleted(ToDoItem entity, Exception exception, ServiceFilterResponse response) {
+
+				if (exception == null) {
+					if (!entity.isComplete()) {
+						mAdapter.add(entity);
+					}
+				} else {
+					createAndShowDialog(exception, "Error");
+				}
+
+			}
+		});
+
+		mTextNewToDo.setText("");
+	}
+
+	/**
+	 * Refresh the list with the items in the Mobile Service Table
+	 */
+	private void refreshItemsFromTable() {
+
+		// Get the items that weren't marked as completed and add them in the
+		// adapter
+		mToDoTable.where().field("complete").eq(val(false)).execute(new TableQueryCallback<ToDoItem>() {
+
+			public void onCompleted(List<ToDoItem> result, int count, Exception exception, ServiceFilterResponse response) {
+				if (exception == null) {
+					mAdapter.clear();
+
+					for (ToDoItem item : result) {
+						mAdapter.add(item);
+					}
+
+				} else {
+					createAndShowDialog(exception, "Error");
+				}
+			}
+		});
+	}
+
+	/**
+	 * Creates a dialog and shows it
+	 *
+	 * @param exception The exception to show in the dialog
+	 * @param title     The dialog title
+	 */
+	private void createAndShowDialog(Exception exception, String title) {
+		Throwable ex = exception;
+		if (exception.getCause() != null) {
+			ex = exception.getCause();
+		}
+		createAndShowDialog(ex.getMessage(), title);
+	}
+
+	/**
+	 * Creates a dialog and shows it
+	 *
+	 * @param message The dialog message
+	 * @param title   The dialog title
+	 */
+	private void createAndShowDialog(String message, String title) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setMessage(message);
+		builder.setTitle(title);
+		builder.create().show();
+	}
+
+	private class ProgressFilter implements ServiceFilter {
+
+		@Override
+		public void handleRequest(ServiceFilterRequest request, NextServiceFilterCallback nextServiceFilterCallback,
+		                          final ServiceFilterResponseCallback responseCallback) {
+			runOnUiThread(new Runnable() {
+
+				@Override
+				public void run() {
+					if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.VISIBLE);
+				}
+			});
+
+			nextServiceFilterCallback.onNext(request, new ServiceFilterResponseCallback() {
+
+				@Override
+				public void onResponse(ServiceFilterResponse response, Exception exception) {
+					runOnUiThread(new Runnable() {
+
+						@Override
+						public void run() {
+							if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.GONE);
+						}
+					});
+
+					if (responseCallback != null) responseCallback.onResponse(response, exception);
+				}
+			});
+		}
+	}
+}
